76e933884eeac9188cdcb7bdc963781879a48f23
brave <brave@qq.com>
2020-07-13 08:22:14 +0800

    brnch

---
+++ day8_control/branch1.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+
+	file, err := os.Open("test.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	data := make([]byte, 100)
+	count, err := file.Read(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("read %d bytes: %q\n", count, data[:count])
+
+}
